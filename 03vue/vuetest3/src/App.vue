2ac011e68ffd2040b89a5ffb1d863bec4d9a9cba
friend5hip <jwy97@naver.com>
2024-09-04 17:17:08 +0900

    Modified vuetest3

--- 03vue/vuetest3/src/App.vue
+++ 03vue/vuetest3/src/App.vue
@@ -1,9 +1,7 @@
 <template>
-  <div>
-    <NavBarView />
-    <SearchBarView />
-    <WeatherView />
-  </div>
+  <NavBarView />
+  <SearchBarView @searchCity="searchCity" />
+  <WeatherView :data="weatherData" />
 </template>
 
 <script setup>
@@ -17,23 +15,31 @@ const weatherData = ref({
   icon: "icon",
   temp: 0,
   text: "text",
-  location: "loc",
-  city: "Seoul",
+  location: "location",
+  city: "jeju",
 });
 
 const getWeather = async () => {
-  try {
-    const res = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${weatherData.value.city}&appid=4eedfeb184dc7cb08af6c0bd529c48b9`
-    );
-    console.log(res.data);
-  } catch (error) {
-    console.log(error);
-  }
+  const res = await axios.get(
+    `https://api.openweathermap.org/data/2.5/weather?q=${weatherData.value.city}&appid=4eedfeb184dc7cb08af6c0bd529c48b9`
+  );
+  console.log(res.data);
+
+  weatherData.value.icon = res.data.weather[0].icon;
+  weatherData.value.temp = res.data.main.temp;
+  weatherData.value.text = res.data.weather[0].description;
+  weatherData.value.location = res.data.sys.country;
+  weatherData.value.city = res.data.name;
 };
 onMounted(() => {
   getWeather();
 });
+
+const searchCity = (city) => {
+  console.log(city);
+  weatherData.value.city = city;
+  getWeather();
+};
 </script>
 
 <style lang="scss" scoped></style>
--- 03vue/vuetest3/src/components/WeatherView.vue
+++ 03vue/vuetest3/src/components/WeatherView.vue
@@ -1,7 +1,35 @@
 <template>
-  <div>weatherview</div>
+  <div class="weatherWrap">
+    <div class="weather-info">
+      <div>
+        <img
+          :src="`https://openweathermap.org/img/wn/${props.data.icon}@4x.png`"
+          alt=""
+        />
+      </div>
+      <div class="temp">{{ (props.data.temp - 273.15).toFixed(1) }}&deg</div>
+      <div class="text">{{ props.data.text }}</div>
+      <div class="location">
+        {{ props.data.location }} / {{ props.data.city }}
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+const props = defineProps({
+  data: Array,
+});
+</script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.weatherWrap {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .weather-info {
+    padding: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+  }
+}
+</style>
